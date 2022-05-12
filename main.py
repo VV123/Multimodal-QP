1f280b39c34cc42ac0b1e72761c94fc99ebd9b65
Wenlu Wang <VV123@users.noreply.github.com>
2022-05-12 14:50:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -197,8 +197,6 @@ if __name__ == '__main__':
     arg_parser.add_argument("--amenLen", default=100, type=int)
     args = arg_parser.parse_args()
 
-    criterion = MSELoss()
-    criterion = L1Loss()
     nce_criterion = MILNCELoss()
 
     text_criterion = nn.CrossEntropyLoss()
